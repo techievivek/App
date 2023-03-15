ea6e294d389209199a5769becc19b011dfe0447a
Alberto <alberto@expensify.com>
2023-03-15 15:07:27 -0400

    add cards

--- src/components/MemoryGame/Cards.js
+++ src/components/MemoryGame/Cards.js
@@ -2,10 +2,20 @@ import Mayor from '../../../assets/images/memoryGame/Mayor.png';
 import BottleGuy from '../../../assets/images/memoryGame/Bottle_guy.png';
 import Gnome from '../../../assets/images/memoryGame/Gnome.png';
 import Back from '../../../assets/images/memoryGame/Back.png';
+import Snake from '../../../assets/images/memoryGame/Snake.png';
+import Space_guy from '../../../assets/images/memoryGame/Space_guy.png';
+import Rocket_guy from '../../../assets/images/memoryGame/Rocket_guy.png';
+import Bones from '../../../assets/images/memoryGame/Bones.png';
+import Carpet_guy from '../../../assets/images/memoryGame/Carpet_guy.png';
 
 export {
     Mayor,
     BottleGuy,
     Gnome,
-    Back
+    Back,
+    Bones,
+    Carpet_guy,
+    Rocket_guy,
+    Snake,
+    Space_guy,
 };
