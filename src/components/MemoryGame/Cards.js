eba91cb7d23ad5eefef6d12b0da27ed05766fe46
Alberto <alberto@expensify.com>
2023-03-15 14:30:58 -0400

    images

--- src/components/MemoryGame/Cards.js
+++ src/components/MemoryGame/Cards.js
@@ -1,7 +1,11 @@
 import Mayor from '../../../assets/images/memoryGame/Mayor.png';
 import BottleGuy from '../../../assets/images/memoryGame/Bottle_guy.png';
+import Gnome from '../../../assets/images/memoryGame/Gnome.png';
+import Back from '../../../assets/images/memoryGame/Back.png';
 
 export {
     Mayor,
     BottleGuy,
-}
+    Gnome,
+    Back,
+};
