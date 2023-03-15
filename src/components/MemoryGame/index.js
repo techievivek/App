5379c19534d08a881e1b53f606a4594ced22e302
Vivek Kumar <vivek@expensify.com>
2023-03-15 14:01:56 -0400

    added game logic: :

--- src/components/MemoryGame/index.js
+++ src/components/MemoryGame/index.js
@@ -6,6 +6,55 @@ import withLocalize, {withLocalizePropTypes} from '../withLocalize';
 
 function App(props) {
     const [shouldTheGameStart, setShouldTheGameStart] = useState(false);
+    const [cards, setCards] = useState(
+      () => shuffleCards(uniqueCardsArray.concat(uniqueCardsArray))
+    );
+    const [openCards, setOpenCards] = useState([]);
+    const [clearedCards, setClearedCards] = useState({});
+    const [moves, setMoves] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const timeout = useRef(null);
+
+    // Check if both the cards have same type. If they do, mark them inactive
+    const evaluate = () => {
+      const [first, second] = openCards;
+      if (cards[first].type === cards[second].type) {
+        setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
+        setOpenCards([]);
+        return;
+      }
+      // Flip cards after a 500ms duration
+      timeout.current = setTimeout(() => {
+        setOpenCards([]);
+      }, 500);
+    };
+
+    const handleCardClick = (index) => {
+      // Have a maximum of 2 items in array at once.
+      if (openCards.length === 1) {
+        setOpenCards((prev) => [...prev, index]);
+        // increase the moves once we opened a pair
+        setMoves((moves) => moves + 1);
+      } else {
+        // If two cards are already open, we cancel timeout set for flipping cards back
+        clearTimeout(timeout.current);
+        setOpenCards([index]);
+      }
+    };
+
+    const checkIsFlipped = (index) => {
+      return openCards.includes(index);
+    };
+
+    const checkIsInactive = (card) => {
+      return Boolean(clearedCards[card.type]);
+    };
+
+    useEffect(() => {
+      if (openCards.length === 2) {
+        setTimeout(evaluate, 500);
+      }
+    }, [openCards]);
 
     return (
       <div className="App">
@@ -17,19 +66,22 @@ function App(props) {
         </header>
         <View style={[styles.mt4]}>
             {shouldTheGameStart ? (
-                <div className="container">
-                {/* {cards.map((card, index) => {
+                <view className="container">
+                {cards.map((card, index) => {
                   return (
                     <Card
                       key={index}
                       card={card}
                       index={index}
                       onClick={handleCardClick}
+                      isDisabled={shouldDisableAllCards}
+                      isInactive={checkIsInactive(card)}
+                      isFlipped={checkIsFlipped(index)}
                     />
                   );
-                })} */}
+                })}
                 The Game
-              </div>
+              </view>
             ) : (
                 <Button onPress={() => setShouldTheGameStart(true)} text={props.translate('cardMemoryGame.startTheGame')} success large />
             )}
